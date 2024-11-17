cfc315d7667f979157790f39a639e5978ce585c5
Brunnosanttos <brunoohsanttos@gmail.com>
2024-11-17 14:00:51 -0300

    db

--- config.php
+++ config.php
@@ -1,10 +1,21 @@
 <?php
+$dbUrl = 'mysql://root:hkojUBzSvbdttLOJCrSZazACRpNdMhTX@junction.proxy.rlwy.net:14560/railway';
 
-    $dbHost = 'mysql://root:hkojUBzSvbdttLOJCrSZazACRpNdMhTX@mysql.railway.internal:3306/railway';
-    $dbUsername = 'root';
-    $dbPassword = '';
-    $dbName = 'form-fifa';
+// Usando parse_url para separar a URL em partes
+$parsedUrl = parse_url($dbUrl);
 
-    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+// Agora podemos usar as partes da URL
+$dbHost = $parsedUrl['host'];       // 'junction.proxy.rlwy.net'
+$dbPort = $parsedUrl['port'];       // '14560'
+$dbUsername = $parsedUrl['user'];   // 'root'
+$dbPassword = $parsedUrl['pass'];   // 'hkojUBzSvbdttLOJCrSZazACRpNdMhTX'
+$dbName = substr($parsedUrl['path'], 1);  // 'railway', remova o '/' inicial
 
+// Conectar ao banco de dados
+$conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName, $dbPort);
+
+// Verificação de erro
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
 ?>
